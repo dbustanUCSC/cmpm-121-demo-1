efb38ec0600eddc220ac0bd78cee0049a7e1faba
dbustanUCSC <dbustan@ucsc.edu>
2023-10-10 11:39:33 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "his game";
-
+//button img
+const buttonEmoj = "😀";
 document.title = gameName;
-
+const button = document.createElement("button");
+//setting button prop
+button.innerHTML = buttonEmoj;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+app.append(button);
