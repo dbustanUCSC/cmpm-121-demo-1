341d45aa568e54a13b44a0ff621f7dabb878c761
dbustanUCSC <dbustan@ucsc.edu>
2023-10-10 21:59:19 -0700

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "his game";
-let counter: number = 0;
+let counter: number = 1;
 const buttonEmoj = "😀";
 document.title = gameName;
 const button = document.createElement("button");
@@ -13,11 +13,20 @@ value.innerHTML = counter.toString() + " smiles...";
 button.addEventListener("mousedown", function () {
     increment();
 });
+let t0 = 0;
+let t1;
+let elapsed;
 function increment() {
-    counter++;
+    t1 = performance.now();
+    elapsed = t1 - t0;
     value.innerHTML = counter.toString() + " smiles...";
+    //we know this is continously calling the same function, thus incrementing it
+    //however we need a way to
+    t0 = performance.now();
+    requestAnimationFrame(increment);
 }
-setInterval(increment, 100);
+//setInterval(increment, 100);
+requestAnimationFrame(increment);
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
