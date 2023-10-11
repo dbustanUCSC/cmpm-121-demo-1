f7ad7d13e0ba73e69c5571cd5177bec00516c608
dbustan <dbustan@ucsc.edu>
2023-10-11 01:09:41 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "his game";
 let counter: number = 1;
+let displayCounter: number;
 const buttonEmoj = "😀";
 document.title = gameName;
 const button = document.createElement("button");
@@ -11,22 +12,25 @@ button.innerHTML = buttonEmoj;
 value.innerHTML = counter.toString() + " smiles...";
 
 button.addEventListener("mousedown", function () {
-    increment();
-});
-//let t0 = 0;
-//let t1;
-//let elapsed;
-function increment() {
-    //t1 = performance.now();
-    //elapsed = t1 - t0;
     counter++;
-    value.innerHTML = counter.toString() + " smiles...";
-    //we know this is continously calling the same function, thus incrementing it
-    //however we need a way to
-    //t0 = performance.now();
+});
+let t0 = 0;
+let t1;
+let elapsed;
+function increment() {
+    t1 = performance.now();
+    elapsed = t1 - t0;
+    //converts it to 0.01 seconds (rather, converts it to the unit of seconds)
+    elapsed *= 1 / 1000;
+    //adds 0.01 seconds to counter
+    counter += elapsed;
+    displayCounter = Math.floor(counter);
+    //here we take the time between two frames, then we add its difference to counter per frame.
+    //through doing this we get 1 unit per second, working by converting milliseconds per difference and adding it
+    value.innerHTML = displayCounter.toString() + " smiles...";
+    t0 = performance.now();
     requestAnimationFrame(increment);
 }
-//setInterval(increment, 100);
 requestAnimationFrame(increment);
 const header = document.createElement("h1");
 header.innerHTML = gameName;
