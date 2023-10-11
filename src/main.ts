c65d989fda23dae8ccc8262c9bbe3e47918468a6
dbustanUCSC <dbustan@ucsc.edu>
2023-10-10 19:18:44 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
-
 const gameName = "his game";
-//button img
+let counter: number = 0;
 const buttonEmoj = "😀";
 document.title = gameName;
 const button = document.createElement("button");
-//setting button prop
+const value = document.createElement("div");
 button.innerHTML = buttonEmoj;
+value.innerHTML = counter.toString() + " smiles...";
+
+button.addEventListener('mousedown', function(){
+    increment();
+});
+function increment(){
+    counter++;
+    value.innerHTML = counter.toString() + " smiles...";
+}
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 app.append(button);
+app.append(value);
