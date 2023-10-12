09af0be6792fc5a6cee424808e6594442354a377
dbustan <dbustan@ucsc.edu>
2023-10-11 22:30:56 -0700

    updated version prior step 5

--- src/main.ts
+++ src/main.ts
@@ -2,38 +2,60 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "his game";
-let counter: number = 1;
-let displayCounter: number;
-const buttonEmoj = "😀";
-document.title = gameName;
-const button = document.createElement("button");
-const value = document.createElement("div");
-button.innerHTML = buttonEmoj;
-value.innerHTML = counter.toString() + " smiles...";
 
-button.addEventListener("mousedown", function () {
+document.title = gameName;
+const smile = document.createElement("button");
+const smileEmoj = "😀";
+smile.innerHTML = smileEmoj;
+const value = document.createElement("div");
+
+let counter: number = 0;
+value.innerHTML = "No smiles...";
+smile.addEventListener("mousedown", function () {
     counter++;
 });
-let t0 = 0;
-let t1;
-let elapsed;
-function increment() {
-    t1 = performance.now();
-    elapsed = t1 - t0;
-    //converts it to 0.01 seconds (rather, converts it to the unit of seconds)
-    elapsed *= 1 / 1000;
-    //adds 0.01 seconds to counter
-    counter += elapsed;
-    displayCounter = Math.floor(counter);
-    //here we take the time between two frames, then we add its difference to counter per frame.
-    //through doing this we get 1 unit per second, working by converting milliseconds per difference and adding it
-    value.innerHTML = displayCounter.toString() + " smiles...";
-    t0 = performance.now();
-    requestAnimationFrame(increment);
-}
-requestAnimationFrame(increment);
+
+const upgrade1Button = document.createElement("button");
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
-app.append(button);
+app.append(smile);
 app.append(value);
+app.append(upgrade1Button);
+
+let costForUpgrade1 = 10;
+let growthRate = 0.0;
+
+let lastTime = 0;
+
+function tick(now: number) {
+    const elapsed = now - lastTime;
+    lastTime = now;
+    counter += (growthRate * elapsed) / 1000;
+
+    //here we take the time between two frames, then we add its difference to counter per frame.
+    //through doing this we get 1 unit per second, working by converting milliseconds per difference of a frame and adding it
+    if (counter > 0) {
+        value.innerHTML = counter.toFixed(2) + " smiles...";
+    }
+    requestAnimationFrame(tick);
+}
+
+requestAnimationFrame(tick);
+
+let upgrade1Emoj = "Please help... 🥸 \n costs: " + costForUpgrade1;
+upgrade1Button.innerHTML = upgrade1Emoj;
+upgrade1Button.addEventListener("mousedown", function () {
+    if (counter >= costForUpgrade1) {
+        upgrade1Purchase();
+    }
+});
+function upgrade1Purchase() {
+    costForUpgrade1 *= 1.15;
+    counter -= costForUpgrade1;
+    upgrade1Emoj = "Please help... 🥸 :). \n costs:" + costForUpgrade1;
+    upgrade1Button.innerHTML = upgrade1Emoj;
+    console.log(costForUpgrade1);
+    growthRate += 0.1;
+}
